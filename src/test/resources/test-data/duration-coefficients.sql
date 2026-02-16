76bbd03dc6bc1c32366b41a1c3e8dfe685572f64
Normalno100 <msomsin@mail.ru>
2026-02-16 07:14:10 +0300

    impl CountryDefaultDayPremium et1

--- src/test/resources/test-data/duration-coefficients.sql
+++
@@ -1,8 +0,0 @@
--- src/test/resources/test-data/duration-coefficients.sql
-INSERT INTO duration_coefficients (days_from, days_to, coefficient, description) VALUES
-(0, 10, 1.0, 'Short trip'),
-(11, 30, 0.95, 'Medium trip - 5% discount'),
-(31, 60, 0.90, 'Long trip - 10% discount'),
-(61, 90, 0.85, 'Extended trip - 15% discount'),
-(91, 180, 0.80, 'Very long trip - 20% discount'),
-(181, 365, 0.75, 'Annual trip - 25% discount');
--- src/test/resources/test-data/medical-risk-limit-levels.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO medical_risk_limit_levels (code, coverage_amount, daily_rate, currency, valid_from) VALUES
-('5000', 5000.00, 1.50, 'EUR', '2025-01-01'),
-('10000', 10000.00, 2.00, 'EUR', '2025-01-01'),
-('20000', 20000.00, 3.00, 'EUR', '2025-01-01'),
-('50000', 50000.00, 4.50, 'EUR', '2025-01-01'),
-('100000', 100000.00, 7.00, 'EUR', '2025-01-01'),
-('200000', 200000.00, 12.00, 'EUR', '2025-01-01')
-ON CONFLICT DO NOTHING;
